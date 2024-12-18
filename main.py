0b905e3a2fe50b1b50455b89b0a71d1dc081561a
Brandon Corfman <bcorfman@fastmail.fm>
2024-12-18 17:15:35 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from taskqueue.example import hello_world
+
+
+def main():
+    hello_world()
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_example.py
@@ -0,0 +1,7 @@
+from taskqueue.example import hello_world
+
+
+def test_hello_world(capsys):
+    hello_world()
+    captured = capsys.readouterr()
+    assert captured.out == "Hello World!\n"    
